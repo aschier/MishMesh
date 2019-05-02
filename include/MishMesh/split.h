358b48aee3abe799ead7c5225574249c06d0f2fa
Alexander Schier <schier@uni-bonn.de>
2019-05-02 15:59:21 +0200

    Added an orig_index property to submeshes generated by submesh or split_connected_components

--- include/MishMesh/split.h
+++ include/MishMesh/split.h
@@ -4,6 +4,6 @@
 #include <set>
 
 namespace MishMesh {
-	MishMesh::TriMesh build_submesh(const MishMesh::TriMesh &input_mesh, const std::set<MishMesh::TriMesh::FaceHandle>& face_set);
-	std::vector<TriMesh> split_connected_components(const TriMesh & input_mesh);
+	MishMesh::TriMesh build_submesh(const MishMesh::TriMesh &input_mesh, const std::set<MishMesh::TriMesh::FaceHandle>& face_set, bool add_original_index_property = false);
+	std::vector<TriMesh> split_connected_components(const TriMesh &input_mesh, bool add_original_index_property = false);
 }
