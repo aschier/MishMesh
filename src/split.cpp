d4ebadce8edc334306339a73dfbdadef57ac1201
Alexander Schier <schier@uni-bonn.de>
2019-05-02 15:59:21 +0200

    Added an orig_index property to submeshes generated by submesh or split_connected_components

--- src/split.cpp
+++ src/split.cpp
@@ -9,24 +9,38 @@ using namespace MishMesh;
  * Build a mesh from a subset of faces of another mesh.
  * @param input_mesh The original mesh.
  * @param face_set A set of faces that should be included in the new mesh.
+ * @param add_original_index_property When set to true, a property "orig_index" is added to the vertices and faces of the mesh.
  * @returns A TriMesh containing only the subset of the faces.
  * @note The method retains the connectivity of the original mesh,
  * but does not transfer any OpenMesh properties or other attributes.
  */
-TriMesh MishMesh::build_submesh(const TriMesh &input_mesh, const set<TriMesh::FaceHandle> &face_set) {
+TriMesh MishMesh::build_submesh(const TriMesh &input_mesh, const set<TriMesh::FaceHandle> &face_set, bool add_original_index_property) {
 	TriMesh submesh;
+	OpenMesh::FPropHandleT<int> prop_orig_face_idx;
+	OpenMesh::VPropHandleT<int> prop_orig_vertex_idx;
+	if(add_original_index_property) {
+		submesh.add_property(prop_orig_face_idx, "orig_index");
+		submesh.add_property(prop_orig_vertex_idx, "orig_index");
+	}
 	map<TriMesh::VertexHandle, TriMesh::VertexHandle> vertex_map;
-	for(auto &f: face_set) {
+	for(auto &fh: face_set) {
 		array<TriMesh::VertexHandle, 3> vhs;
 		short i = 0;
-		for(auto v_it = input_mesh.cfv_ccwbegin(f); v_it != input_mesh.cfv_ccwend(f); v_it++) {
+		for(auto v_it = input_mesh.cfv_ccwbegin(fh); v_it != input_mesh.cfv_ccwend(fh); v_it++) {
 			assert(i < 3);
 			if(vertex_map.find(*v_it) == vertex_map.end()) {
-				vertex_map[*v_it] = submesh.add_vertex(input_mesh.point(*v_it));
+				const auto vh = submesh.add_vertex(input_mesh.point(*v_it));
+				vertex_map[*v_it] = vh;
+				if(add_original_index_property) {
+					submesh.property(prop_orig_vertex_idx, vh) = v_it->idx();
+				}
 			}
 			vhs[i++] = vertex_map[*v_it];
 		}
-		submesh.add_face(vhs.data(), 3);
+		const auto new_fh = submesh.add_face(vhs.data(), 3);
+		if(add_original_index_property) {
+			submesh.property(prop_orig_face_idx, new_fh) = fh.idx();
+		}
 	}
 	return submesh;
 }
@@ -34,9 +48,10 @@ TriMesh MishMesh::build_submesh(const TriMesh &input_mesh, const set<TriMesh::Fa
 /**
  * Split the mesh into its connected components.
  * @param input_mesh the mesh.
+ * @param add_original_index_property When set to true, a property "orig_index" is added to the vertices and faces of the mesh.
  * @returns A vector with one mesh per connected component.
  */
-std::vector<TriMesh> MishMesh::split_connected_components(const TriMesh &input_mesh) {
+std::vector<TriMesh> MishMesh::split_connected_components(const TriMesh &input_mesh, bool add_original_index_property) {
 	vector<TriMesh> result_meshes;
 	set<TriMesh::FaceHandle> faces;
 	for(auto f : input_mesh.faces()) {
@@ -48,7 +63,7 @@ std::vector<TriMesh> MishMesh::split_connected_components(const TriMesh &input_m
 			faces.erase(f);
 		}
 		TriMesh component_mesh;
-		component_mesh = build_submesh(input_mesh, component_faces);
+		component_mesh = build_submesh(input_mesh, component_faces, add_original_index_property);
 		result_meshes.push_back(component_mesh);
 	}
 	return result_meshes;
