840a7b619466c9bd9a35eb2f9c2f43eeb6da174b
Alexander Schier <schier@uni-bonn.de>
2019-07-25 14:17:49 +0200

    Changed parameter name in compute_projected_origins from v2x to edge_length

--- include/MishMesh/geodesics.h
+++ include/MishMesh/geodesics.h
@@ -4,5 +4,6 @@
 
 namespace MishMesh {
 	typedef OpenMesh::VPropHandleT<double> GeodesicDistanceProperty;
+
 	void compute_geodesics(TriMesh &mesh, const TriMesh::VertexHandle start_vh, const GeodesicDistanceProperty geodesicDistanceProperty);
 }
