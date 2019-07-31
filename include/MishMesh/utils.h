5391d611b98a8389e1363731986e1938d8e82099
Alexander Schier <schier@uni-bonn.de>
2019-07-31 16:25:15 +0200

    Added a bounding box implementation
    Added a bounding box implementation, that supports different vector
    types and dimensions.

--- include/MishMesh/utils.h
+++ include/MishMesh/utils.h
@@ -11,6 +11,68 @@ namespace MishMesh {
 	double compute_area(const TriMesh &mesh, const std::array<TriMesh::VertexHandle, 3> vertices);
 	double compute_area(const TriMesh &mesh, const TriMesh::FaceHandle fh);
 
+	/** A simple bounding box defined by the left-top(-far) and right-bottom(-near) point
+	 * @tparam VectorT A vector class, that stores DIM entries and provides an operator[] for the entries.
+	 * @tparam DIM The dimension of the point and the bounding box.
+	 */
+	template<typename VectorT = OpenMesh::Vec3d, unsigned int DIM = 3>
+	struct BBox {
+		VectorT ltf;
+		VectorT rbn;
+		/**
+		 * A BBox is valid, if all left-top(-far) values are smaller than the corresponding right-bottom(-near) values.
+		 * @returns true, when the bounding box is valid.
+		 */
+		bool is_valid() const {
+			for(unsigned int j = 0; j < DIM; j++){
+				if(ltf[j] > rbn[j]) {
+					return false;
+				}
+			}
+			return true;
+		}
+		/**
+		 * Make the bounding box valid, by swapping min/max coordinates, that are in the wrong order,
+		 * i.e. when bbox.ltf[j] < bbox.rbn[j], bbox.ltf[j] and bbox.rbn[j] are swapped.
+		 */
+		void make_valid() {
+			for(unsigned int j = 0; j < DIM; j++){
+				if(ltf[j] > rbn[j]) {
+					std::swap(ltf[j], rbn[j]);
+				}
+			}
+		}
+		/**
+		 * Check if a point is in a given bounding box.
+		 * @param point the point.
+		 * @param bbox The bounding box.
+		 * @returns true, when the point is inside the bounding box and false otherwise.
+		 */
+		bool contains(const VectorT point) const {
+			for(unsigned int j = 0; j < DIM; j++){
+				if(point[j] > rbn[j] || point[j] < ltf[j]){
+					return false;
+				}
+			}
+			return true;
+		}
+		/**
+		 * Clip a point to a bounding box.
+		 * @param point The point.
+		 * @returns The point, if it is inside the bounding box or a clipped point, when the point was outside the box.
+		 */
+		VectorT clip(VectorT point) const {
+			for(unsigned int j = 0; j < DIM; j++){
+				if(point[j] > rbn[j]){
+					point[j] = rbn[j];
+				} else if(point[j] < ltf[j]){
+					point[j] = ltf[j];
+				}
+			}
+			return point;
+		}
+	};
+
 	template double compute_area(const std::array<OpenMesh::VectorT<double, 2>, 3> points);
 	template double compute_area(const std::array<OpenMesh::VectorT<double, 3>, 3> points);
 }
