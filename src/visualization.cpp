67a576e42f66df9fc6762198b7d49e54a4dba15c
Alexander Schier <schier@uni-bonn.de>
2019-08-02 11:55:01 +0200

    Added visualization functions for generating regular grids

--- src/visualization.cpp
+++ src/visualization.cpp
@@ -64,10 +64,10 @@ TriMesh MishMesh::vertex_mesh(const TriMesh &mesh, std::vector<TriMesh::VertexHa
 		}
 		for(short k = 0; k < 6; k++) {
 			vector<TriMesh::VertexHandle> face_vec{
-				vhs[cube_indices[k][0]],
-				vhs[cube_indices[k][1]],
+				vhs[cube_indices[k][3]],
 				vhs[cube_indices[k][2]],
-				vhs[cube_indices[k][3]]
+				vhs[cube_indices[k][1]],
+				vhs[cube_indices[k][0]]
 			};
 			auto fh = vertexMesh.add_face(face_vec);
 		}
@@ -94,4 +94,77 @@ void MishMesh::colorize_mesh(MishMesh::TriMesh &mesh, const OpenMesh::VPropHandl
 		double value = mesh.property(vertexProperty, vh);
 		mesh.set_color(vh, {255 * value / (max_value - min_value), 0, 0});
 	}
+}
+
+/**
+ * Generate a regular grid of a bounding box defined by its top-left-far and right-bottom-near vertices.
+ * @param resolution The number of grid points in each dimension.
+ * @param bbox_ltf The left-top-far vertex of the bounding box.
+ * @param bbox_rbn The right-bottom-near vertex of the bounding box.
+ * @param point_size The size of a cube at a grid point. Use 0 to use 1/10 of the width of a grid cell.
+ * @returns A vertex mesh with cubes at the grid points.
+ */
+TriMesh MishMesh::grid_mesh(const int resolution[3], const double bbox_ltf[3], const double bbox_rbn[3], double point_size) {
+	MishMesh::TriMesh point_mesh;
+	vector<MishMesh::TriMesh::VertexHandle> vertices;
+	vertices.reserve(resolution[0] * resolution[1] * resolution[2]);
+	for(int i = 0; i < resolution[0]; i++) {
+		for(int j = 0; j < resolution[1]; j++) {
+			for(int k = 0; k < resolution[2]; k++){
+				OpenMesh::Vec3d point{
+					bbox_ltf[0] + (bbox_rbn[0] - bbox_ltf[0]) / resolution[0] * i,
+					bbox_ltf[1] + (bbox_rbn[1] - bbox_ltf[1]) / resolution[0] * j,
+					bbox_ltf[2] + (bbox_rbn[2] - bbox_ltf[2]) / resolution[0] * k,
+				};
+				vertices.push_back(point_mesh.add_vertex(point));
+			}
+		}
+	}
+	if(point_size == 0){
+		point_size = abs(bbox_rbn[0] - bbox_ltf[0]) / resolution[0] / 10.0;
+	}
+	return MishMesh::vertex_mesh(point_mesh, vertices, point_size);
+}
+
+inline int grid_cell_index(const int resolution[3], const int i, const int j, const int k) {
+	return i + resolution[0] * j + resolution[0] * resolution[1] * k;
+}
+
+/**
+ * Visualize the grid near a isosurface of a signed distance function, by generating a regular grid and only
+ * showing the vertices that have at least one neighbor for which the function value the opposite sign.
+ * @param resolution The number of grid points in each dimension.
+ * @param bbox_ltf The left-top-far vertex of the bounding box.
+ * @param bbox_rbn The right-bottom-near vertex of the bounding box.
+ * @param point_values The function values on the grid vertices, using the index format x + y*resolution[0] + z*resolution[0]*resolution[1].
+ * @param point_size The size of a cube at a grid point. Use 0 to use 1/10 of the width of a grid cell.
+ * @returns A vertex mesh with cubes at the grid points.
+ */
+TriMesh MishMesh::isosurface_grid_mesh(const int resolution[3], const double bbox_ltf[3], const double bbox_rbn[3], std::vector<double> &point_values, double point_size) {
+	MishMesh::TriMesh point_mesh;
+	vector<MishMesh::TriMesh::VertexHandle> vertices;
+	for(int i = 1; i < resolution[0] - 1; i++) {
+		for(int j = 1; j < resolution[1] - 1; j++) {
+			for(int k = 1; k < resolution[2] - 1; k++){
+				if(
+					point_values[grid_cell_index(resolution, i + 1, j, k)] * point_values[grid_cell_index(resolution, i, j, k)] <= 0 ||
+					point_values[grid_cell_index(resolution, i - 1, j, k)] * point_values[grid_cell_index(resolution, i, j, k)] <= 0 ||
+					point_values[grid_cell_index(resolution, i, j + 1, k)] * point_values[grid_cell_index(resolution, i, j, k)] <= 0 ||
+					point_values[grid_cell_index(resolution, i, j - 1, k)] * point_values[grid_cell_index(resolution, i, j, k)] <= 0 ||
+					point_values[grid_cell_index(resolution, i, j, k + 1)] * point_values[grid_cell_index(resolution, i, j, k)] <= 0 ||
+					point_values[grid_cell_index(resolution, i, j, k - 1)] * point_values[grid_cell_index(resolution, i, j, k)] <= 0
+					) {
+					OpenMesh::Vec3d p = OpenMesh::Vec3d{
+						bbox_ltf[0] + i * (bbox_rbn[0] - bbox_ltf[0]) / resolution[0],
+						bbox_ltf[1] + j * (bbox_rbn[1] - bbox_ltf[1]) / resolution[1],
+						bbox_ltf[2] + k * (bbox_rbn[2] - bbox_ltf[2]) / resolution[2]};
+					vertices.push_back(point_mesh.add_vertex(p));
+				}
+			}
+		}
+	}
+	if(point_size == 0){
+		point_size = abs(bbox_rbn[0] - bbox_ltf[0]) / resolution[0] / 10.0;
+	}
+	return MishMesh::vertex_mesh(point_mesh, vertices, point_size);
 }
