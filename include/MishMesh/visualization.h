fa4e5367f041ee6e80d42ca04c92f8d450fe366f
Alexander Schier <schier@uni-bonn.de>
2019-09-02 10:39:30 +0200

    Added MeshT template parameter to add_box to allow both for TriMesh and PolyMesh (with quad faces)

--- include/MishMesh/visualization.h
+++ include/MishMesh/visualization.h
@@ -4,8 +4,9 @@
 #include <vector>
 
 namespace MishMesh{
+	template<typename MeshT>
+	void add_box(MeshT &mesh, const BBox<OpenMesh::Vec3d, 3> box);
 	TriMesh edge_mesh(const TriMesh &mesh, std::vector<TriMesh::EdgeHandle> edge_handles, double thickness);
-	void add_box(TriMesh &mesh, BBox<OpenMesh::Vec3d, 3> box);
 	TriMesh vertex_mesh(const TriMesh &mesh, std::vector<TriMesh::VertexHandle> vertex_handles, double size);
 	void colorize_mesh(MishMesh::TriMesh &mesh, const OpenMesh::VPropHandleT<double> &vertexProperty);
 	TriMesh grid_mesh(const int resolution[3], const double bbox_ltf[3], const double bbox_rbn[3], const double point_size = 0);
