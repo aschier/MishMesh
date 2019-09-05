1b4d96661364b7ee3dc1560dd49b8f9d1fc58a1c
Alexander Schier <schier@uni-bonn.de>
2019-09-05 13:29:36 +0200

    Improved the performance of get_connected_faces
    Implemented get_connected_faces like get_connected_vertices and
    add faces to the result when adding them to the queue, what avoids
    adding a face more than once to the queue.

--- src/search.cpp
+++ src/search.cpp
@@ -11,14 +11,14 @@ using namespace MishMesh;
  * @returns A set of all faces reachable from the input face and the input face itself.
  */
 set<TriMesh::FaceHandle> MishMesh::get_connected_faces(const TriMesh &input_mesh, const TriMesh::FaceHandle start_face){
-	set<TriMesh::FaceHandle> faces{};
+	set<TriMesh::FaceHandle> faces{start_face};
 	list<TriMesh::FaceHandle> queue{start_face};
 	while(!queue.empty()){
 		auto face = queue.front();
 		queue.pop_front();
-		faces.insert(face);
 		for(auto f_it = input_mesh.cff_begin(face); f_it != input_mesh.cff_end(face); f_it++){
 			if(faces.find(*f_it) != faces.end()) continue;
+			faces.insert(*f_it);
 			queue.push_back(*f_it);
 		}
 	}
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,8 @@
 #include "MishMesh/utils.h"
 #include <MishMesh/macros.h>
 
+#include <MishMesh/TriMesh.h>
+
 using namespace std;
 
 namespace MishMesh {
