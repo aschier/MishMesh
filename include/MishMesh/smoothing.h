f93ee9f1df3df10251cca5f937085bbf8693451b
Alexander Schier <schier@uni-bonn.de>
2019-09-05 14:00:35 +0200

    Added mesh type template parameter to smooth_mesh

--- include/MishMesh/smoothing.h
+++ include/MishMesh/smoothing.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <MishMesh/TriMesh.h>
 #include <OpenMesh/Tools/Smoother/JacobiLaplaceSmootherT.hh>
 
 namespace MishMesh {
-	typedef OpenMesh::Smoother::JacobiLaplaceSmootherT<TriMesh> SmootherT;
-	void smooth_mesh(TriMesh &mesh, const int iterations = 1, const SmootherT::Component component = SmootherT::Component::Tangential_and_Normal);
+	template<typename MeshT>
+	using SmootherT = OpenMesh::Smoother::JacobiLaplaceSmootherT<MeshT>;
+	template<typename MeshT>
+	void smooth_mesh(MeshT &mesh, const int iterations = 1, const typename SmootherT<MeshT>::Component component = SmootherT<MeshT>::Component::Tangential_and_Normal);
 }
--- src/smoothing.cpp
+++ src/smoothing.cpp
@@ -1,5 +1,8 @@
 #include "MishMesh/smoothing.h"
 
+#include <MishMesh/TriMesh.h>
+#include <MishMesh/PolyMesh.h>
+
 /**
  * Apply laplacian smoothing to a mesh by moving all vertices to the average of their neighbor's positions.
  * @param[inout] mesh The mesh to smooth.
@@ -8,8 +11,12 @@
  * @note This is a simple interface to OpenMesh::Smoother. Use the OpenMesh functions directly,
  * when you need more control over the smoothing process.
  */
-void MishMesh::smooth_mesh(MishMesh::TriMesh &mesh, const int iterations, const MishMesh::SmootherT::Component component) {
-	SmootherT smoother(mesh);
-	smoother.initialize(component, OpenMesh::Smoother::JacobiLaplaceSmootherT<TriMesh>::Continuity::C0);
+template<typename MeshT>
+void MishMesh::smooth_mesh(MeshT &mesh, const int iterations, const typename MishMesh::SmootherT<MeshT>::Component component) {
+	SmootherT<MeshT> smoother(mesh);
+	smoother.initialize(component, OpenMesh::Smoother::JacobiLaplaceSmootherT<MeshT>::Continuity::C0);
 	smoother.smooth(iterations);
 }
+
+template void MishMesh::smooth_mesh(MishMesh::TriMesh &mesh, const int iterations, const MishMesh::SmootherT<MishMesh::TriMesh>::Component component);
+template void MishMesh::smooth_mesh(MishMesh::PolyMesh &mesh, const int iterations, const MishMesh::SmootherT<MishMesh::PolyMesh>::Component component);
