9b0c19394dbf675ee916c3211c24f7113d5dcd30
Alexander Schier <schier@uni-bonn.de>
2019-09-30 14:04:35 +0200

    Added a euler_characteristic method in utils.h

--- include/MishMesh/utils.h
+++ include/MishMesh/utils.h
@@ -11,4 +11,9 @@ namespace MishMesh {
 	double compute_area(const std::array<OpenMesh::VectorT<double, DIM>, 3> points);
 	double compute_area(const TriMesh &mesh, const std::array<TriMesh::VertexHandle, 3> vertices);
 	double compute_area(const TriMesh &mesh, const TriMesh::FaceHandle fh);
+
+	template<typename MeshT>
+	inline size_t euler_characteristic(const MeshT &mesh) {
+		return mesh.n_vertices() - mesh.n_edges() + mesh.n_faces();
+	}
 }
