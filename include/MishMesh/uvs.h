7029c741de9b788956836c587a5d1b5d4fec0c20
Alexander Schier <schier@uni-bonn.de>
2025-08-26 17:57:55 +0200

    Add split_with_uvs function
    Add a function that splits an edge and interpolates the UV coordinates
    at the split point.

--- include/MishMesh/uvs.h
+++ include/MishMesh/uvs.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "MishMesh/TriMesh.h"
 #include "MishMesh/PolyMesh.h"
+#include "MishMesh/utils.h"
 
 namespace MishMesh {
-	void flip_with_uvs(MishMesh::TriMesh & mesh, const MishMesh::TriMesh::EdgeHandle &eh);
+	void flip_with_uvs(MishMesh::TriMesh &mesh, const MishMesh::TriMesh::EdgeHandle &eh);
+	SmartHalfedgePair split_with_uvs(MishMesh::TriMesh &mesh, const OpenMesh::SmartHalfedgeHandle heh, const double t);
 	void triangulate_with_uvs(MishMesh::PolyMesh &mesh);
 }
--- src/uvs.cpp
+++ src/uvs.cpp
@@ -1,4 +1,5 @@
 #include "MishMesh/uvs.h"
+#include "MishMesh/utils.h"
 
 namespace MishMesh {
 	/**
@@ -16,6 +17,61 @@ namespace MishMesh {
 		auto heh2 = mesh.opposite_halfedge_handle(heh1);
 		mesh.set_texcoord2D(heh1, mesh.texcoord2D(mesh.prev_halfedge_handle(mesh.opposite_halfedge_handle(heh1))));
 		mesh.set_texcoord2D(heh2, mesh.texcoord2D(mesh.prev_halfedge_handle(mesh.opposite_halfedge_handle(heh2))));
+	}
+
+	/**
+	 * Split an edge and interpolate the UV coordinates for all halfedges adjacent to the split
+	 * vertex. The UVs at the halfedge and its opposite halfedge are interpolated separately,
+	 * such that UV boundaries are preserved.
+	 *
+	 * The edge and halfedge properties are copied to the new segments using mesh.copy_all_properties
+	 * and for the inserted triangulation edges and new faces all properties but the UV coordinates are
+	 * undefined.
+	 *
+	 * @param mesh The mesh.
+	 * @param heh The boundary halfedge handle to split.
+	 * @param t the relative position in [0, 1] where to split the edge.
+	 * @returns A pair of halfedge handles representing the two segments of the input halfedge.
+	 * @note the mesh is assumed to have a texcoord2D halfedge property that stores the
+	 *       current UVs.
+	 * @note The original halfedge becomes one of the two segments. The new vertex can be retrieved
+	 *       with result.first.to().
+	 */
+	SmartHalfedgePair split_with_uvs(MishMesh::TriMesh &mesh, const OpenMesh::SmartHalfedgeHandle heh, const double t) {
+		const auto p = (1.0 - t) * mesh.point(heh.from()) + t * mesh.point(heh.to());
+		auto vh = mesh.add_vertex(p);
+
+		auto interpolate_uv = [&](const auto &heh, const double t) {
+			return (1.0 - t) * mesh.texcoord2D(heh.prev()) + t * mesh.texcoord2D(heh);
+		};
+
+		MishMesh::TriMesh::TexCoord2D edge_from_uv = mesh.texcoord2D(heh.opp());
+		MishMesh::TriMesh::TexCoord2D edge_to_uv = mesh.texcoord2D(heh);
+		MishMesh::TriMesh::TexCoord2D path_from_uv = mesh.texcoord2D(heh.next());
+		MishMesh::TriMesh::TexCoord2D path_to_uv = mesh.texcoord2D(heh.opp().next());
+		MishMesh::TriMesh::TexCoord2D interpolated_uv = interpolate_uv(heh, t);
+		MishMesh::TriMesh::TexCoord2D interpolated_uv_opp = interpolate_uv(heh.opp(), 1 - t);
+
+		auto [edge_heh1, edge_heh2] = MishMesh::split_edge(mesh, heh, vh);
+		const auto &new_heh = (edge_heh1 == heh) ? edge_heh2 : edge_heh1;
+
+		mesh.copy_all_properties(heh.edge(), new_heh.edge());
+		mesh.copy_all_properties(heh, new_heh);
+		mesh.copy_all_properties(heh.opp(), new_heh.opp());
+
+		// Direction: current_vh = edge_heh2.prev().from() -> new_vh -> edge_heh2.opp().next().to()
+		auto triangulation_heh1 = edge_heh2.prev();
+		auto triangulation_heh2 = edge_heh2.opp().next();
+
+		mesh.set_texcoord2D(edge_heh1, interpolated_uv);
+		mesh.set_texcoord2D(edge_heh1.opp(), edge_from_uv);
+		mesh.set_texcoord2D(edge_heh2, edge_to_uv);
+		mesh.set_texcoord2D(edge_heh2.opp(), interpolated_uv_opp);
+		mesh.set_texcoord2D(triangulation_heh1, interpolated_uv);
+		mesh.set_texcoord2D(triangulation_heh1.opp(), path_from_uv);
+		mesh.set_texcoord2D(triangulation_heh2, path_to_uv);
+		mesh.set_texcoord2D(triangulation_heh2.opp(), interpolated_uv_opp);
+		return {edge_heh1, edge_heh2};
 	}
 
 	/**
